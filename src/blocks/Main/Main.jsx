284edac39c7415e5855cedb00fb8a5ab167670c1
UtbanovAG <utbanovag@it2g.ru>
2021-12-26 17:14:06 +0300

    update main

--- src/blocks/Main/Main.jsx
+++ src/blocks/Main/Main.jsx
@@ -3,7 +3,7 @@ import styles from "./Main.module.css";
 export const Main = () => {
     return (
         <section>
-            <h1>Medival hackaton участвуй срочно</h1>
+            <h1>Medival hackaton участвуй срочно срочно срочно</h1>
         </section>
     )
 }
